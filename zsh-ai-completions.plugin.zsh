ab5251045c3f3ee945d9015dbdb32aa05c8ffffe
Michael Bianco <iloveitaly@gmail.com>
2025-02-20 11:02:03 -0700

    refactor: streamline completion path setup
    Generated-by: aiautocommit

--- zsh-ai-completions.plugin.zsh
+++ zsh-ai-completions.plugin.zsh
@@ -1,12 +1,1 @@
-#!/usr/bin/env zsh
-
-# date +%H:%M:%S.%N   # profiling info
-
-# Fetch $0 according to plugin standard proposed at:
-# http://zdharma.org/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
-0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
-
-CURRENT_DIR=${0:A:h}
-COMPLETIONS_PATH="$CURRENT_DIR/completions"
-
-fpath+=$ZSH_COMPLETION_GENERATOR_DIR
+fpath+="${0:A:h}/src"
